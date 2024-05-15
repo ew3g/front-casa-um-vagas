0513bc43d977e50685c65cfcfd420f26268572f4
Edilson Rodrigues <edilson.w3g@gmail.com>
2024-05-15 14:11:45 -0300

    feat: tags

--- src/components/Vagas.js
+++ src/components/Vagas.js
@@ -19,6 +19,17 @@ const Vagas = () => {
     const [sort, setSort] = useState("dataPublicacao");
     const maxResults = 10;
 
+    const getTags = (text) => {
+        const array = text.split(",");
+        // .map(function(item) {
+        //     return item.trim();
+        // });
+
+        console.log(array);
+
+        return array;
+    };
+
     const fetchVagas = async (pg, max, srt) => {
         console.log(pg, max, srt);
         //const usuarioId = localStorage.getItem("usuarioId");
@@ -143,12 +154,12 @@ const Vagas = () => {
                                                 {v.cargo} - Publicado em: {v.dataPublicacao}
                                                 <div id="right">
                                                     <div id="edit-item" className="my-clickable">
-                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16" onClick={() => handleEditarVaga(v.id)}>
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil" viewBox="0 0 16 16" onClick={() => handleEditarVaga(v.id)}>
                                                                 <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
                                                             </svg>
                                                     </div>
                                                     <div id="delete-item" className="my-clickable">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-square-fill" viewBox="0 0 16 16" onClick={() => handleExcluirVaga(v.id)}>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-square-fill" viewBox="0 0 16 16" onClick={() => handleExcluirVaga(v.id)}>
                                                             <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708"/>
                                                         </svg>
                                                     </div>
@@ -164,9 +175,15 @@ const Vagas = () => {
                                                 <span>Descrição: {v.descricao}</span><br />
                                                 <span>Empresa: {v.nomeEmpresa}</span><br />
                                                 <span>Cidade: {v.cidade}</span><br />
-                                                <span>Habilidade Requiridas: {v.habilidadesRequeridas}</span><br />
+                                                {/* <span>Habilidade Requiridas: {v.habilidadesRequeridas}</span><br /> */}
 
-
+                                                {getTags(v.habilidadesRequeridas).map((h, idx) => {
+                                                    console.log(v.habilidadesRequeridas)
+                                                    var i = `id${idx}`;
+                                                    return <span key={i} className="badge bg-secondary">&nbsp;{h}</span>
+                                                    
+                                                })}
+                                                
                                                 {/* <div className="d-grid gap-2 mt-3">
                                                     <button type="button" className="btn btn-primary" onClick={() => handleEditarVaga(v.id)}>
                                                         Editar
